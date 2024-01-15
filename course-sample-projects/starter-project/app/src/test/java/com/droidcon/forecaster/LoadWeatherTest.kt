d066180f508b8308b2a4aa5b8161c211847f252c
Jov Mit <mitrejcevski@gmail.com>
2024-01-15 10:38:13 +0100

    We have captured the case when there is no weather data available

---
+++ course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/LoadWeatherTest.kt
@@ -0,0 +1,43 @@
+package com.droidcon.forecaster
+
+import com.google.common.truth.Truth.*
+import org.junit.jupiter.api.Test
+
+class LoadWeatherTest {
+
+    @Test
+    fun noWeatherAvailable() {
+        val location = "location"
+
+        val result = fetchWeatherFor(location)
+
+        assertThat(result).isEqualTo(WeatherData.Empty)
+    }
+
+    private fun fetchWeatherFor(location: String): WeatherData {
+        return WeatherData.Empty
+    }
+
+    data class WeatherData(
+        val city: String,
+        val country: String,
+        val region: String,
+        val temperature: Int,
+        val iconUrl: String,
+        val description: String,
+        val feelsLike: Int
+    ) {
+
+        companion object {
+            val Empty = WeatherData(
+                city = "",
+                country = "",
+                region = "",
+                temperature = 0,
+                iconUrl = "",
+                description = "",
+                feelsLike = 0
+            )
+        }
+    }
+}
