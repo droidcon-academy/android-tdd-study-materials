3d8a9b273f4ab356ef77a10a22d4bf61466f9e0f
Jov Mit <mitrejcevski@gmail.com>
2024-01-15 11:28:58 +0100

    We removed irrelevant details to improve test intention

--- course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/LoadWeatherTest.kt
+++ course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/LoadWeatherTest.kt
@@ -1,6 +1,6 @@
 package com.droidcon.forecaster
 
-import com.droidcon.forecaster.LoadWeatherTest.WeatherDataBuilder.Companion.aWeatherData
+import com.droidcon.forecaster.WeatherDataBuilder.Companion.aWeatherData
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
@@ -21,73 +21,13 @@ class LoadWeatherTest {
         val rotterdam = "Rotterdam"
         val weatherInRotterdam = aWeatherData()
             .withCity("Rotterdam")
-            .withCountry("Netherlands")
-            .withRegion("South Holland")
             .withTemperature(20)
-            .withDescription("Clear")
-            .withFeelsLike(18)
             .build()
-
         val weatherViewModel = WeatherViewModel()
 
         val result = weatherViewModel.fetchWeatherFor(rotterdam)
 
         assertThat(result).isEqualTo(weatherInRotterdam)
-    }
-
-    class WeatherDataBuilder {
-
-        private var city: String = ""
-        private var country: String = ""
-        private var region: String = ""
-        private var temperature: Int = 0
-        private var iconUrl: String = ""
-        private var description: String = ""
-        private var feelsLike: Int = 0
-
-        fun withCity(city: String) = apply {
-            this.city = city
-        }
-
-        fun withCountry(country: String) = apply {
-            this.country = country
-        }
-
-        fun withRegion(region: String) = apply {
-            this.region = region
-        }
-
-        fun withTemperature(temperature: Int) = apply {
-            this.temperature = temperature
-        }
-
-        fun withIconUrl(iconUrl: String) = apply {
-            this.iconUrl = iconUrl
-        }
-
-        fun withDescription(description: String) = apply {
-            this.description = description
-        }
-
-        fun withFeelsLike(feelsLike: Int) = apply {
-            this.feelsLike = feelsLike
-        }
-
-        fun build(): WeatherData {
-            return WeatherData(
-                city,
-                country,
-                region,
-                temperature,
-                iconUrl,
-                description,
-                feelsLike
-            )
-        }
-
-        companion object {
-            fun aWeatherData() = WeatherDataBuilder()
-        }
     }
 
     class WeatherViewModel {
@@ -96,12 +36,12 @@ class LoadWeatherTest {
             if (location == "Rotterdam") {
                 return WeatherData(
                     "Rotterdam",
-                    "Netherlands",
-                    "South Holland",
+                    "",
+                    "",
                     20,
                     "",
-                    "Clear",
-                    18
+                    "",
+                    0
                 )
             }
             return WeatherData.Empty
---
+++ course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/WeatherDataBuilder.kt
@@ -0,0 +1,56 @@
+package com.droidcon.forecaster
+
+class WeatherDataBuilder {
+
+    private var city: String = ""
+    private var country: String = ""
+    private var region: String = ""
+    private var temperature: Int = 0
+    private var iconUrl: String = ""
+    private var description: String = ""
+    private var feelsLike: Int = 0
+
+    fun withCity(city: String) = apply {
+        this.city = city
+    }
+
+    fun withCountry(country: String) = apply {
+        this.country = country
+    }
+
+    fun withRegion(region: String) = apply {
+        this.region = region
+    }
+
+    fun withTemperature(temperature: Int) = apply {
+        this.temperature = temperature
+    }
+
+    fun withIconUrl(iconUrl: String) = apply {
+        this.iconUrl = iconUrl
+    }
+
+    fun withDescription(description: String) = apply {
+        this.description = description
+    }
+
+    fun withFeelsLike(feelsLike: Int) = apply {
+        this.feelsLike = feelsLike
+    }
+
+    fun build(): LoadWeatherTest.WeatherData {
+        return LoadWeatherTest.WeatherData(
+            city,
+            country,
+            region,
+            temperature,
+            iconUrl,
+            description,
+            feelsLike
+        )
+    }
+
+    companion object {
+        fun aWeatherData() = WeatherDataBuilder()
+    }
+}
