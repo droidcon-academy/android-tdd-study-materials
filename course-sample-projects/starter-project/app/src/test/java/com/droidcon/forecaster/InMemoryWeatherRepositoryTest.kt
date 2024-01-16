a611d3e72d766e112f5149ecdd934fdb8e42649f
Jov Mit <mitrejcevski@gmail.com>
2024-01-16 13:08:33 +0100

    Extract data from abstractions

--- course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/InMemoryWeatherRepositoryTest.kt
+++ course-sample-projects/starter-project/app/src/test/java/com/droidcon/forecaster/InMemoryWeatherRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.droidcon.forecaster
 
 import com.droidcon.forecaster.WeatherDataBuilder.Companion.aWeatherData
+import com.droidcon.forecaster.data.WeatherData
 import com.droidcon.forecaster.data.WeatherResult
 import com.google.common.truth.Truth.*
 import kotlinx.coroutines.test.runTest
@@ -8,12 +9,12 @@ import org.junit.jupiter.api.Test
 
 class InMemoryWeatherRepositoryTest {
 
+    private val location = "location"
+    private val weatherAtLocation = aWeatherData().withCity(location).build()
+
     @Test
     fun weatherAvailable() = runTest {
-        val location = "location"
-        val weatherAtLocation = aWeatherData().withCity(location).build()
-        val weatherForLocation = mapOf(location to weatherAtLocation)
-        val repository = InMemoryWeatherRepository(weatherForLocation)
+        val repository = weatherRepositoryWith(location, weatherAtLocation)
 
         val result = repository.loadWeatherFor(location)
 
@@ -22,10 +23,7 @@ class InMemoryWeatherRepositoryTest {
 
     @Test
     fun noWeatherAvailable() = runTest {
-        val location = "location"
-        val weatherAtLocation = aWeatherData().withCity(location).build()
-        val weatherForLocation = mapOf("anythingBut$location" to weatherAtLocation)
-        val repository = InMemoryWeatherRepository(weatherForLocation)
+        val repository = weatherRepositoryWithout(location, weatherAtLocation)
 
         val result = repository.loadWeatherFor(location)
 
@@ -34,12 +32,27 @@ class InMemoryWeatherRepositoryTest {
 
     @Test
     fun loadingWeatherError() = runTest {
-        val location = "location"
-        val noWeatherAvailableForLocation = mapOf(location to null)
-        val repository = InMemoryWeatherRepository(noWeatherAvailableForLocation)
+        val missingWeatherAtLocation = null
+        val repository = weatherRepositoryWith(location, missingWeatherAtLocation)
 
         val result = repository.loadWeatherFor(location)
 
         assertThat(result).isEqualTo(WeatherResult.Error)
+    }
+
+    private fun weatherRepositoryWith(
+        location: String,
+        weatherAtLocation: WeatherData?
+    ): WeatherRepository {
+        val weatherForLocation = mapOf(location to weatherAtLocation)
+        return InMemoryWeatherRepository(weatherForLocation)
+    }
+
+    private fun weatherRepositoryWithout(
+        location: String,
+        weatherAtLocation: WeatherData?
+    ): WeatherRepository {
+        val weatherForLocation = mapOf("anythingBut$location" to weatherAtLocation)
+        return InMemoryWeatherRepository(weatherForLocation)
     }
 }
