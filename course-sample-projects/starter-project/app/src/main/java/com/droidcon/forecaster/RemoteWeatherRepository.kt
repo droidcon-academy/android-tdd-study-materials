4693d42b816d34d6cbb3dc9fb3c8ade616c59a62
Jov Mit <mitrejcevski@gmail.com>
2024-01-16 15:34:08 +0100

    Improve the remote weather repository by moving mapping out

--- course-sample-projects/starter-project/app/src/main/java/com/droidcon/forecaster/RemoteWeatherRepository.kt
+++ course-sample-projects/starter-project/app/src/main/java/com/droidcon/forecaster/RemoteWeatherRepository.kt
@@ -1,8 +1,8 @@
 package com.droidcon.forecaster
 
-import com.droidcon.forecaster.data.WeatherData
-import com.droidcon.forecaster.data.WeatherResponse
 import com.droidcon.forecaster.data.WeatherResult
+import com.droidcon.forecaster.data.toResult
+import com.droidcon.forecaster.data.toWeatherData
 import retrofit2.HttpException
 
 class RemoteWeatherRepository(
@@ -15,23 +15,7 @@ class RemoteWeatherRepository(
             val weatherData = response.toWeatherData()
             WeatherResult.Loaded(weatherData)
         } catch (httpException: HttpException) {
-            if (httpException.code() == 404) {
-                WeatherResult.Error
-            } else if (httpException.code() == 400) {
-                WeatherResult.Unavailable
-            } else {
-                throw httpException
-            }
+            httpException.toResult()
         }
     }
-
-    private fun WeatherResponse.toWeatherData() = WeatherData(
-        city = location.name,
-        country = location.country,
-        region = location.region,
-        temperature = current.temperature,
-        iconUrl = current.icons.first(),
-        description = current.descriptions.joinToString(),
-        feelsLike = current.feelsLike
-    )
 }
