b9f887754bec602ec9a645b179b35504cc6329a7
Jov Mit <mitrejcevski@gmail.com>
2024-01-16 16:03:37 +0100

    Aligened levels of abstraction

--- course-sample-projects/starter-project/app/src/main/java/com/droidcon/forecaster/WeatherViewModel.kt
+++ course-sample-projects/starter-project/app/src/main/java/com/droidcon/forecaster/WeatherViewModel.kt
@@ -11,13 +11,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
 
 class WeatherViewModel(
     private val weatherRepository: WeatherRepository,
     private val queryLengthValidator: QueryLengthValidator,
     private val backgroundDispatcher: CoroutineDispatcher
-): ViewModel() {
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(WeatherScreenState())
     val uiState: StateFlow<WeatherScreenState> = _uiState.asStateFlow()
@@ -36,22 +35,28 @@ class WeatherViewModel(
         }
     }
 
+    private fun updateScreenStateFor(weatherResult: WeatherResult) {
+        when (weatherResult) {
+            is WeatherResult.Unavailable -> updateScreenStateWithWeatherUnavailableError()
+            is WeatherResult.Error -> updateScreenStateWithLoadingError()
+            is WeatherResult.Loaded -> updateScreenStateWithWeatherData(weatherResult)
+        }
+    }
+
     private fun updateScreenStateWithLoading() {
         _uiState.update { it.copy(isLoading = true) }
     }
 
-    private fun updateScreenStateFor(weatherResult: WeatherResult) {
-        when (weatherResult) {
-            is WeatherResult.Unavailable -> {
-                _uiState.update { it.copy(isLoading = false, isWeatherUnavailable = true) }
-            }
-            is WeatherResult.Error -> {
-                _uiState.update { it.copy(isLoading = false, isWeatherLoadingError = true) }
-            }
-            is WeatherResult.Loaded -> {
-                _uiState.update { it.copy(isLoading = false, weatherData = weatherResult.data) }
-            }
-        }
+    private fun updateScreenStateWithWeatherUnavailableError() {
+        _uiState.update { it.copy(isLoading = false, isWeatherUnavailable = true) }
+    }
+
+    private fun updateScreenStateWithLoadingError() {
+        _uiState.update { it.copy(isLoading = false, isWeatherLoadingError = true) }
+    }
+
+    private fun updateScreenStateWithWeatherData(weatherResult: WeatherResult.Loaded) {
+        _uiState.update { it.copy(isLoading = false, weatherData = weatherResult.data) }
     }
 
     private fun updateScreenStateWithBadQuery() {
